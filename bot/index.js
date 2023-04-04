b6f4a86382817dcd7ede66bad0c021a2c5022ed3
Olawale Olayinka Joseph <57357459+olinks@users.noreply.github.com>
2023-04-04 22:44:24 +0100

    update transaction value check

--- bot/index.js
+++ bot/index.js
@@ -56,6 +56,7 @@ async function main(pk) {
 
   let camelot_route = "0xeb034303A3C4380Aa78b14B86681bd0bE730De1C";
   let initial_lottery_number = randomGen(10);
+  console.log("initialised winning lottery number =>", initial_lottery_number);
 
   // Arbi Rush contract address
   const arbiRushAddress = "0xb70c114B20d1EE068Dd4f5F36E301d0B604FEC18";
@@ -104,6 +105,7 @@ async function main(pk) {
   }
 
   async function pingIdleGroup(idleTimeSeconds) {
+    const { jackpot_reward } = await getJackpotInfo();
     const { usd_value, marketcap, eth_usd_price } = await getDexScreenerData();
     const bot_data = {
       rush_usd: usd_value,
@@ -182,6 +184,14 @@ async function main(pk) {
       }
   }
 
+    const getJackpotInfo = async () => {
+    const jackpot_balance = await getAddressBalance(provider, jackpotAddress);
+    const jackpot_reward = jackpot_balance / 2;
+    const next_jackpot = jackpot_reward / 2;
+    const third_jackpot = jackpot_reward / 2 / 2;
+    return { jackpot_balance, jackpot_reward, next_jackpot, third_jackpot };
+  };
+
   async function getDexScreenerData() {
     const response = await axios.get(
       "https://api.dexscreener.com/latest/dex/tokens/0xb70c114B20d1EE068Dd4f5F36E301d0B604FEC18"
@@ -227,8 +237,8 @@ async function main(pk) {
     try {
       const { usd_value, marketcap, eth_value, eth_usd_price } =
         await getDexScreenerData();
-      let eth_spent = no_tokens * eth_value;
-      let usd_spent = (no_tokens * usd_value) + ((no_tokens * usd_value) * 0.12);
+      let eth_spent = (no_tokens * eth_value) + ((no_tokens * eth_value) * 0.121);
+      let usd_spent = (no_tokens * usd_value) + ((no_tokens * usd_value) * 0.121);
 
       // if the tokens are coming from the Camelot router and not going back to the contract address
       //  but an actual wallet then its a buy
@@ -242,61 +252,61 @@ async function main(pk) {
         let winner = false;
 
         // $100 => 1%
-        if (lottery_value >= 100 && lottery_value <= 200) {
+        if (lottery_value > 99 && lottery_value < 199) {
           lottery_number = randomGen(100);
           lottery_percentage = 1;
           console.log("1% buy lottery number =>", lottery_number);
         }
         // $200 => 2%
-        else if (lottery_value >= 200 && lottery_value <= 300) {
+        else if (lottery_value > 199 && lottery_value < 299) {
           lottery_number = randomGen(90);
           lottery_percentage = 2;
           console.log("2% buy lottery number =>", lottery_number);
         }
         // $300 => 3%
-        else if (lottery_value >= 300 && lottery_value <= 400) {
+        else if (lottery_value > 299 && lottery_value < 399) {
           lottery_number = randomGen(80);
           lottery_percentage = 3;
           console.log("3% buy lottery number =>", lottery_number);
         }
         // $400 => 4%
-        else if (lottery_value >= 400 && lottery_value <= 500) {
+        else if (lottery_value > 399 && lottery_value < 499) {
           lottery_number = randomGen(70);
           lottery_percentage = 4;
           console.log("4% buy lottery number =>", lottery_number);
         }
         // $500 => 5%
-        else if (lottery_value >= 500 && lottery_value <= 600) {
+        else if (lottery_value > 499 && lottery_value < 599) {
           lottery_number = randomGen(60);
           lottery_percentage = 5;
           console.log("5% buy lottery number =>", lottery_number);
         }
         // $600 => 6%
-        else if (lottery_value >= 600 && lottery_value <= 700) {
+        else if (lottery_value > 599 && lottery_value < 699) {
           lottery_number = randomGen(50);
           lottery_percentage = 6;
           console.log("6% buy lottery number =>", lottery_number);
         }
         // $700 => 7%
-        else if (lottery_value >= 700 && lottery_value <= 800) {
+        else if (lottery_value > 699 && lottery_value < 799) {
           lottery_number = randomGen(40);
           lottery_percentage = 7;
           console.log("7% buy lottery number =>", lottery_number);
         }
         // $800 => 8%
-        else if (lottery_value >= 800 && lottery_value <= 900) {
+        else if (lottery_value > 799 && lottery_value < 899) {
           lottery_number = randomGen(30);
           lottery_percentage = 8;
           console.log("8% buy lottery number =>", lottery_number);
         }
         // $900 => 9%
-        else if (lottery_value >= 900 && lottery_value <= 1000) {
+        else if (lottery_value > 899 && lottery_value < 999) {
           lottery_number = randomGen(20);
           lottery_percentage = 9;
           console.log("9% buy lottery number =>", lottery_number);
         }
         // $1000 => 10%
-        else if (lottery_value >= 1000) {
+        else if (lottery_value > 999 || lottery_value >= 1000) {
           lottery_number = randomGen(10);
           lottery_percentage = 10;
           console.log("10% buy lottery number =>", lottery_number);
@@ -308,7 +318,7 @@ async function main(pk) {
 
         // Dummy amount set here
         // setLastBuyCountdown(listener_to, 10000)
-
+        console.log("Current winning lottery number =>", initial_lottery_number);
         jackpot_reward = jackpot_balance / 2
         // Check if winner
         winner = await checkWinner(lottery_number, listener_to, jackpot_reward);
