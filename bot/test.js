b0a700f91c6f91b402455d6f6a9a1a1c136d2a7c
Olawale Olayinka Joseph <57357459+olinks@users.noreply.github.com>
2023-04-17 19:31:13 +0100

    changed number of emojis, added function to fetch ETH price, set bot to send message every 30 mins, jackpot max $10K,  jackpot 40%

--- bot/test.js
+++ bot/test.js
@@ -55,9 +55,9 @@ app.use(express.static(__dirname + "/static"));
 // });
 async function get(){
 
-  const transactionData = await provider.getTransactionReceipt("0x14c2f65c76855567d5c0da0131d90f89e09ad9d703c3cf3e41ec67e97bcd240b");
+  // const transactionData = await provider.getTransactionReceipt("0x14c2f65c76855567d5c0da0131d90f89e09ad9d703c3cf3e41ec67e97bcd240b");
 
-  console.log (JSON.stringify((Number(transactionData.logs[0].data)/(10 ** 18)),null,4));
+  // console.log (JSON.stringify((Number(transactionData.logs[0].data)/(10 ** 18)),null,4));
   
   // const decodedInput = inter.parseTransaction({ data: transactionData.data, value: transactionData.value});
   
@@ -77,6 +77,53 @@ async function get(){
   //         });
 
           // console.log("\nArgs Number", Number(decodedInput.args[0].hex));
+          const testObject = {
+    "from": "0xeb034303A3C4380Aa78b14B86681bd0bE730De1C",
+    "to": "0xb70c114B20d1EE068Dd4f5F36E301d0B604FEC18",
+    "value": "30.288948384523385006",
+    "data": {
+        "blockNumber": 80429865,
+        "blockHash": "0x3fca992614cdcb641a5544eb0a222b4a877d2908372cb02da9c6fb904293f7ee",
+        "transactionIndex": 1,
+        "removed": false,
+        "address": "0xb70c114B20d1EE068Dd4f5F36E301d0B604FEC18",
+        "data": "0x000000000000000000000000000000000000000000000001a457f613f7a878ae",
+        "topics": [
+            "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+            "0x000000000000000000000000eb034303a3c4380aa78b14b86681bd0be730de1c",
+            "0x000000000000000000000000b70c114b20d1ee068dd4f5f36e301d0b604fec18"
+        ],
+        "transactionHash": "0x616376de3eadfa30924d76204f9f9ab5412182c61805206614eaa2eff97b058b",
+        "logIndex": 12,
+        "event": "Transfer",
+        "eventSignature": "Transfer(address,address,uint256)",
+        "args": [
+            "0xeb034303A3C4380Aa78b14B86681bd0bE730De1C",
+            "0xb70c114B20d1EE068Dd4f5F36E301d0B604FEC18",
+            {
+                "type": "BigNumber",
+                "hex": "0x01a457f613f7a878ae"
+            }
+        ]
+    }
+}
+
+console.log(JSON.stringify(testObject.data.transactionHash,null,4));
+
+  async function getEthUsdPrice() {
+    try{
+      const response = await axios.get(
+        `https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=USD`
+      );
+
+      console.log(response.data.ethereum.usd);
+
+    }catch (e){
+      console.log("Error Reaching Dexscreener API");
+      return cached_dexscreener_data;
+    }
+  }
+  getEthUsdPrice();
 }
 
 get();
