9be6241a3958c5c775d0b33dbca11234728f7a4f
Olawale Olayinka Joseph <57357459+olinks@users.noreply.github.com>
2023-05-04 23:50:49 +0100

    catching buys

--- lottery.js
+++ lottery.js
@@ -351,7 +351,7 @@ async function startLottery(pk) {
       // if the tokens are coming from the Camelot router and not going back to the contract address
       //  but an actual wallet then its a buy
 
-      if ((from == routerLiquidityPairAddress || to != routerLiquidityPairAddress) && (to != tokenContactAddress || to != "0x1111111254eeb25477b68fb85ed929f73a960582"|| to != "0x64768A3a2453F1E8DE9e43e92D65Fc36E4c9872d" )) {
+      if ((from == routerLiquidityPairAddress || to != routerLiquidityPairAddress || from != "0x1111111254eeb25477b68fb85ed929f73a960582" || from != "0x1144bcc225335b07b1239c78e9801164c4419e38" ) && (to != tokenContactAddress || to != "0x1111111254eeb25477b68fb85ed929f73a960582"|| to != "0x64768A3a2453F1E8DE9e43e92D65Fc36E4c9872d" )) {
       // if (from != tokenContactAddress) {
         // ##############################################################################################################################
         //  GETTING ETH VALUES
